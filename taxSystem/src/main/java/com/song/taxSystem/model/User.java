951195d3f836dd3928f2498e36463a91dff4ddae
Eunsong Koh <eunsongkohh@gmail.com>
2024-04-17 18:30:08 -0700

    added user.java

--- taxSystem/src/main/java/com/song/taxSystem/model/User.java
+++ taxSystem/src/main/java/com/song/taxSystem/model/User.java
@@ -6,38 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class User {
+public class User{
     @Id
-    // makes it such that it automatically increments the ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
-
-    public User() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- taxSystem/src/main/java/com/song/taxSystem/repository/UserRepository.java
+++ taxSystem/src/main/java/com/song/taxSystem/repository/UserRepository.java
@@ -1,3 +1,10 @@
 package com.song.taxSystem.repository;
 
-import com.song.taxSystem.model.User
+import com.song.taxSystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
